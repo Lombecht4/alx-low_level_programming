b01afd89937bc4212daedb66dba19348e27e66f8
lombecht4 <lombecht4@gmail.com>
2023-02-20 17:26:28 -0800

    fourth commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * *main - Entry point
+ * *Return: Always  (success)
+ * */
+
+int main(void)
+(
+        puts("\"Prpgramming is like building a multiligual puzzle");
+	                return (0);
+)			
