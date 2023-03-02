4109942ff29ee03794c27bc06b80eb7d9f29cae2
lombecht4 <lombecht4@gmail.com>
2023-03-02 15:52:38 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,20 +6,19 @@
  * Return: A pointer to the encoded string.
  */
 
-char *leet(char *s)
+char *leet(char *str)
 {
-int i, j;
-char *a = "aAeEOotTlL";
-char *b = "4433007711";
-for (i = 0; s[i] != '\0'; i++)
+int indx1 = 0, indx2;
+char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+while (str[indx1])
 {
-for (j = 0; j < 10; j++)
+for (indx2 = 0; indx2 <= 7; indx2++)
 {
-if (s[i] == a[j])
-{
-s[i] = b[j];
+if (str[indx1] == leet[indx2] ||
+str[indx1] - 32 == leet[indx2])
+str[indx1] = indx2 + '0';
 }
+indx1++;
 }
-}
-return (s);
+return (str);
 }
