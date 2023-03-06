e6a675209daf87ef11a0208022b2f6a0f7854b2f
lombecht4 <lombecht4@gmail.com>
2023-03-06 15:16:31 -0800

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,26 @@
+
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _memset - Entry point
- * @s: pointed destination
- * @b: constant byte
- * @n: bytes
- * :Return: ALways 0 (success)
+ * *_memset - fills memory with a constant byte,
+ * @s: address to memory area.
+ * @b: char to be used.
+ * @n: number of bytes to the memory block.
+ * Return: the pointer to dest.
  */
 
-char *_memset(char *s, char b, unsigned int n);
-{
-unsigned int i;
-for (i = 0; i < n; i++)
-s[i] = b;
-return (s);
+char *_memset(char *s, char b, unsigned int n)
 
+{
+
+unsigned int i;
+for (i = 0; n > 0; i++, n--)
+
+{
+
+	s[i] = b;
+
+}
+return (s);
 }
--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,30 +1,14 @@
-#include "main.h"
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-        unsigned int i;
 
-        i = 0;
-        while (i < size)
-        {
-                if (i % 10)
-                {
-                        printf(" ");
-                }
-                if (!(i % 10) && i)
-                {
-                        printf("\n");
-                }
-                printf("0x%02x", buffer[i]);
-                i++;
-        }
-        printf("\n");
+int _putchar(char c)
+
+{
+	return (write(1, &c, 1));
 }
